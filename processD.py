afd03105f0adb3ae647c4ab2c90aa59e494023f3
Sakib Rahman <rahmans@bison.local>
2019-04-07 03:53:35 -0500

    python wrapper coded successfully

--- processD.py
+++ processD.py
@@ -5,7 +5,6 @@ import ROOT as R
 
 
 T=R.TChain("T")
-T.Add(sys.argv[1])
 
-
-T.Process("/home/rahmans/analysis/BeamSteeringAnalysis/plane/processD.C+",sys.argv[2]+","+sys.argv[3]+","+sys.argv[4])
+T.Add(sys.argv[1])    ## Source Root File
+T.Process("/home/rahmans/analysis/TargetAnalysis/processD.C+",sys.argv[2]+","+sys.argv[3]+","+sys.argv[4])   ## Destination Root Folder, tag, generator
